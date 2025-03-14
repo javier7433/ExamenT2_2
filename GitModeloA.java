c8a608433bc8c58c7db8d4c3aa36f9a4a717983b
javier7433 <javiergfm2005@gmail.com>
2025-03-14 13:28:53 +0100

    atras

--- GitModeloA.java
+++ GitModeloA.java
@@ -3,7 +3,6 @@ public class GitModeloA {
 
 	public static void main(String[] args) {
 		System.out.println("Hola mundo");
-		System.out.println("REMOTO");
 		System.out.println("Hola a tod@s");
 	}
 }
