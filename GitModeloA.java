d1a08e78e17e5dd23e51e5a653faaee5f4d22dff
javier7433 <javiergfm2005@gmail.com>
2025-03-14 13:21:47 +0100

    Remoto

--- GitModeloA.java
+++ GitModeloA.java
@@ -3,7 +3,7 @@ public class GitModeloA {
 
 	public static void main(String[] args) {
 		System.out.println("Hola mundo");
-		System.out.println();
+		System.out.println("REMOTO");
 		System.out.println("Hola a tod@s");
 	}
 }
